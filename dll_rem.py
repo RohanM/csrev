f61bb17a8b5562e7502c7961ced010123a26f4fe
Rohan Mitchell <rohan@rohanmitchell.com>
2010-11-25 14:27:54 +1100

    Added tests for DLL

--- dll_rem.py
+++ dll_rem.py
@@ -2,13 +2,32 @@ class Node:
     prev = next = None
     value = None
 
-    def __init__(self, value):
+    def __init__(self, value, prev, next):
         self.value = value
+        self.prev = prev
+        self.next = next
+
 
 class DLL:
     head = None
 
+    def add(self, value):
+        """ Add a value to the end of the list. """
+        # Find the tail
+        tail = self.head
+        while tail and tail.next:
+            tail = tail.next
+
+        if tail:
+            # Add a new node with the value
+            tail.next = Node(value, tail, None)
+        else:
+            # Add first node to the list
+            self.head = Node(value, None, None)
+
+
     def remove_recursive(self, value, node=None):
+        """ Remove a node using a recursive algorithm. """
         if node == None:
             node = self.head
 
@@ -24,7 +43,8 @@ class DLL:
 
 
     def remove_flat(self, value):
-        node = head
+        """ Remove a node using a flat (non-recursive) algorithm. """
+        node = self.head
         while node and node.value != value:
             node = node.next
 
@@ -36,5 +56,67 @@ class DLL:
             if node.next:
                 node.next.prev = node.prev
 
-dll = DLL()
+    def as_list(self):
+        """ Convert the linked list into a python list. """
+        l = []
+        node = self.head
+        while node:
+            l.append(node.value)
+            node = node.next
+        return l
+
+
+
+##########################
+# Test remove_recursive()
+##########################
+
+l = DLL()
+
+l.add(1)
+l.add(2)
+l.add(3)
+l.add(4)
+l.add(5)
+
+assert(l.as_list() == [1, 2, 3, 4, 5])
+
+# Remove first
+l.remove_recursive(1)
+assert(l.as_list() == [2, 3, 4, 5])
+
+# Remove middle
+l.remove_recursive(4)
+assert(l.as_list() == [2, 3, 5])
+
+# Remove last
+l.remove_recursive(5)
+assert(l.as_list() == [2, 3])
+
+
+##########################
+# Test remove_flat()
+##########################
+
+l = DLL()
+
+l.add(1)
+l.add(2)
+l.add(3)
+l.add(4)
+l.add(5)
+
+assert(l.as_list() == [1, 2, 3, 4, 5])
+
+# Remove first
+l.remove_flat(1)
+assert(l.as_list() == [2, 3, 4, 5])
+
+# Remove middle
+l.remove_flat(4)
+assert(l.as_list() == [2, 3, 5])
+
+# Remove last
+l.remove_flat(5)
+assert(l.as_list() == [2, 3])
 
